3388d9412c61228ed020313891f1e1029e6490d4
nswbmw <4279697+nswbmw@users.noreply.github.com>
2026-07-17 21:27:39 +0800

    feat: v2.1.1 - fix: proxy validation - chore: update dependencies

--- src/node/Adyen.js
+++ src/node/Adyen.js
@@ -22,6 +22,8 @@ class Adyen {
           this.agent = new HttpsProxyAgent(proxy)
         } else if (proxy.startsWith('socks://')) {
           this.agent = new SocksProxyAgent(proxy)
+        } else {
+          throw new Error(`Invalid proxy string: "${proxy}". Expected "http://..." or "socks://..."`)
         }
       } else if (typeof proxy === 'object') {
         if (!['http', 'socks'].includes(proxy.protocol)) {
